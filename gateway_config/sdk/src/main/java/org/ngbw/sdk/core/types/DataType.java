14f9ac2d8e06a03c9f94d80a6a8a6eea739f2786
Mona Wong <mona@sdsc.edu>
2017-01-23 11:10:15 -0800

    Added IMAGE, TXT, ZIP and PDF for our gateway

---
+++ gateway_config/sdk/src/main/java/org/ngbw/sdk/core/types/DataType.java
@@ -0,0 +1,29 @@
+/*
+ * DataType.java
+ */
+package org.ngbw.sdk.core.types;
+
+
+/**
+ * 
+ * @author Roland H. Niedner
+ * @author Paul Hoover
+ * 
+ */
+public enum DataType {
+	BLAST_OUTPUT,
+	FASTA_OUTPUT,
+    IMAGE,
+	MATRIX,
+    PDF,
+	PHYLOGENETIC_TREE,
+	SEQUENCE,
+	SEQUENCE_ALIGNMENT,
+	SERIALIZED_BINARY,
+	SIMILARITY_SEARCH_HIT,
+	SIMILARITY_SEARCH_RESULT,
+	STRUCTURE,
+    TEXT,
+	UNKNOWN,
+    ZIP
+}
