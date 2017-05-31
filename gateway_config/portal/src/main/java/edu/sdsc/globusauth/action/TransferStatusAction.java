1c713bf9b4945c873ca0ed9c87fbff358add9128
Mona Wong <mona@sdsc.edu>
2017-05-31 12:55:53 -0700

    updated call to profilemanager with additional arguments; added UTC and display size in human readable unit

--- gateway_config/portal/src/main/java/edu/sdsc/globusauth/action/TransferStatusAction.java
+++ gateway_config/portal/src/main/java/edu/sdsc/globusauth/action/TransferStatusAction.java
@@ -7,6 +7,7 @@ import org.globusonline.transfer.Authenticator;
 import org.globusonline.transfer.GoauthAuthenticator;
 import org.globusonline.transfer.JSONTransferAPIClient;
 import org.json.JSONArray;
+import org.ngbw.sdk.WorkbenchSession;
 import org.ngbw.web.actions.NgbwSupport;
 
 import java.time.LocalDate;
@@ -51,12 +52,16 @@ public class TransferStatusAction extends NgbwSupport {
             taskmap.put("task_id", taskId);
             taskmap.put("source_endpoint_display_name", r.document.getString("source_endpoint_display_name"));
             taskmap.put("destination_endpoint_display_name", r.document.getString("destination_endpoint_display_name"));
-            taskmap.put("request_time", r.document.getString("request_time"));
+            taskmap.put("request_time", r.document.getString("request_time")+" UTC");
             taskmap.put("status", r.document.getString("status"));
-            try {
-                taskmap.put("completion_time", r.document.getString("completion_time"));
+			try {
+                String c_time = r.document.getString("completion_time");
+                if (c_time != null)
+                    taskmap.put("completion_time", c_time+" UTC");
+                else
+                    taskmap.put("completion_time", "");
             } catch (org.json.JSONException e) {
-                taskmap.put("completion_time", "Not available");
+                taskmap.put("completion_time", "");
             }
             taskmap.put("files_transferred", r.document.getInt("files_transferred"));
             taskmap.put("faults", r.document.getInt("faults"));
@@ -68,7 +73,7 @@ public class TransferStatusAction extends NgbwSupport {
             taskmap.put("files", r.document.getInt("files"));
             taskmap.put("directories", r.document.getInt("directories"));
             taskmap.put("files_skipped", r.document.getInt("files_skipped"));
-            taskmap.put("bytes_transferred", r.document.getLong("bytes_transferred"));
+	    	taskmap.put("bytes_transferred", humanReadableByteCount(r.document.getLong("bytes_transferred"),true));
             statuslist.add(taskmap);
 
         } else {
@@ -85,12 +90,16 @@ public class TransferStatusAction extends NgbwSupport {
                     taskmap.put("task_id", data.getJSONObject(i).getString("task_id"));
                     taskmap.put("source_endpoint_display_name", data.getJSONObject(i).getString("source_endpoint_display_name"));
                     taskmap.put("destination_endpoint_display_name", data.getJSONObject(i).getString("destination_endpoint_display_name"));
-                    taskmap.put("request_time", data.getJSONObject(i).getString("request_time"));
+                    taskmap.put("request_time", data.getJSONObject(i).getString("request_time")+" UTC");
                     taskmap.put("status", data.getJSONObject(i).getString("status"));
-                    try {
-                        taskmap.put("completion_time", data.getJSONObject(i).getString("completion_time"));
+					try {
+                        String c_time = data.getJSONObject(i).getString("completion_time");
+                        if (c_time != null)
+                            taskmap.put("completion_time", c_time+" UTC");
+                        else
+                            taskmap.put("completion_time", "");
                     } catch (org.json.JSONException e) {
-                        taskmap.put("completion_time", "Not available");
+                        taskmap.put("completion_time", "");
                     }
                     taskmap.put("files_transferred", data.getJSONObject(i).getInt("files_transferred"));
                     taskmap.put("faults", data.getJSONObject(i).getInt("faults"));
@@ -102,7 +111,7 @@ public class TransferStatusAction extends NgbwSupport {
                     taskmap.put("files", data.getJSONObject(i).getInt("files"));
                     taskmap.put("directories", data.getJSONObject(i).getInt("directories"));
                     taskmap.put("files_skipped", data.getJSONObject(i).getInt("files_skipped"));
-                    taskmap.put("bytes_transferred", data.getJSONObject(i).getLong("bytes_transferred"));
+		    taskmap.put("bytes_transferred", humanReadableByteCount(data.getJSONObject(i).getLong("bytes_transferred"),true));
                     statuslist.add(taskmap);
                 }
             }
@@ -114,12 +123,28 @@ public class TransferStatusAction extends NgbwSupport {
         Long user_id = (Long) getSession().get("user_id");
         List<String> tr = profileManager.loadRecord(user_id);
         if (tr != null && tr.size() > 0) {
+            String dest_path = ( String ) getSession().get
+                ( OauthConstants.DEST_ENDPOINT_PATH );
+            WorkbenchSession wbs = ( WorkbenchSession ) getSession().get
+                ( "workbenchSession" );
             for (String taskid: tr)
-                profileManager.updateRecord(txaction.updateTask(taskid,client));
+            {
+                //profileManager.updateRecord(txaction.updateTask(taskid,client));
+                profileManager.updateRecord (
+                    txaction.updateTask ( taskid,client ), dest_path, wbs );
+            }
         }
 
         return SUCCESS;
 
+    }
+
+    private String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.2f %sB", bytes / Math.pow(unit, exp), pre);
     }
 
     //public Map<String,Object> getTaskmap() {return taskmap;}
