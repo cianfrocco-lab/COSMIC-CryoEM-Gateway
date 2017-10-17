c429e27232a9a985157f8f266ef6d76277914036
Mona Wong <mona@sdsc.edu>
2017-10-17 10:23:24 -0700

    added STAR data type

---
+++ gateway_config/sdk/src/main/java/org/ngbw/sdk/core/types/DataFormat.java
@@ -0,0 +1,38 @@
+/*
+ * DataFormat.java
+ */
+package org.ngbw.sdk.core.types;
+
+
+/**
+ * 
+ * @author Roland H. Niedner
+ * @author Paul Hoover
+ * 
+ */
+public enum DataFormat {
+	BLAST_TEXT,
+	BLAST_XML,
+	CLUSTAL,
+	EMBL,
+	EMBL_XML,
+	FASTOUT,
+	FASTA,
+	GENBANK,
+	GENPEPT,
+	HENNIG86,
+	HTML,
+	MMCIF,
+	MSF,
+	NEEDLE,
+	NEWICK,
+	NEXUS,
+	PDB,
+	PDB_FINDER,
+	PHYLIP,
+	SIRIUSWB,
+    STAR,
+	UNIPROT,
+	UNIPROT_XML,
+	UNKNOWN
+}
