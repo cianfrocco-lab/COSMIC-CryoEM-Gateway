37d13d76735ca2e1cccbc8cbc9369c88b9e1d021
Mona Wong <mona@sdsc.edu>
2018-03-15 14:22:40 -0700

    added including particles.star 1 subdirectory deep in transferred directory (github issue #78)

--- gateway_config/portal/src/main/java/edu/sdsc/globusauth/controller/Transfer2DataManager.java
+++ gateway_config/portal/src/main/java/edu/sdsc/globusauth/controller/Transfer2DataManager.java
@@ -8,6 +8,7 @@
 package edu.sdsc.globusauth.controller;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
@@ -109,42 +110,29 @@ public class Transfer2DataManager extends NgbwSupport
         // Loop through all the transferred directories
         for ( String transferred_dir : transferred_dirs )
         {
-            // First, look for star file in the top-level transferred
+            // First, look for *.star file in the top-level transferred
             // directories...
             //log.debug ( "MONA : transferred_dir = " + transferred_dir );
             String full_path = destination_path + transferred_dir + "/";
             //log.debug ( "MONA : full_path = " + full_path );
             File dir = new File ( full_path );
-            //log.debug ( "MONA : dir getTotalSpace = " + dir.getTotalSpace() );
-            //log.debug ( "MONA : dir sizeOf = " + FileUtils.sizeOf ( dir ) );
-            //log.debug ( "MONA : dir sizeOfDirectory = " + FileUtils.sizeOfDirectory ( dir ) );
+            //log.debug ( "MONA : dir = " + dir );
             Collection < File > files = FileUtils.listFiles ( dir,
                 starfile_ext, false );
             //log.debug ( "MONA : files = " + files );
 
-            // If found, just use the first one...
             if ( files != null && ! files.isEmpty() )
             {
                 //log.debug ( "MONA : files.size() = " + files.size() );
-                //File file = ( File ) files.iterator().next();
                 for ( File file : files )
                 {
                     //log.debug ( "MONA : file = " + file );
                     //log.debug ( "MONA : file name = " + file.getName() );
-                    //log.debug ( "MONA : file.length() = " + file.length() );
-                    //long size = FileUtils.sizeOf ( file );
-                    //log.debug ( "MONA : FileUtils.sizeOf = " + FileUtils.sizeOf ( file ) );
-                    //log.debug ( "MONA : FileUtils.sizeOfDirectory = " + FileUtils.sizeOfDirectory ( file ) );
                     String label = file.toString().substring
                         ( label_start_index );
                     //log.debug ( "MONA : label = " + label );
                     try
                     {
-                        /*
-                        data_item = new UserDataDirItem ( folder, tr_id,
-                            transferred_dir + "/" + file.getName(),
-                            file.length() );
-                        */
                         data_item = new UserDataDirItem ( folder, tr_id, label,
                             FileUtils.sizeOf ( dir ) );
                         //log.debug ( "MONA : data_item = " + data_item );
@@ -169,44 +157,38 @@ public class Transfer2DataManager extends NgbwSupport
                 }
             }
 
-            /*
-            // Else look for star file in the subdirectories
-            else
-            {
-                files = FileUtils.listFiles ( dir, starfile_ext, true );
-                log.debug ( "MONA : files = " + files );
-
-                // If no * /*.star found, then go back to next transferred
-                // directory...
-                if ( files == null || files.isEmpty() )
-                    continue;
-                log.debug ( "MONA : files.size() = " + files.size() );
-
-                for ( File file : files )
+            // Now, look for particles.star file only one directory down...
+            //files = FileUtils.listFiles ( dir, starfile_ext, true );
+            String[] subdirs = dir.list ( new FilenameFilter()
                 {
-                    log.debug ( "MONA : file = " + file );
-                    log.debug ( "MONA : file 1 = " + file.toString() );
-                    /* debug
-                    log.debug ( "MONA : file 2 = " + file.getPath() );
-                    log.debug ( "MONA : file 3 = " + file.getParent() );
-                    log.debug ( "MONA : file 4 = " + file.getName() );
-                    try
+                    @Override
+                    public boolean accept ( File current, String name )
                     {
-                        log.debug ( "MONA : file 5 = " + file.getCanonicalPath() );
+                        return new File ( current, name ).isDirectory();
                     }
-                    catch ( Exception e )
-                    {
-                    }
-                    log.debug ( "MONA : file 6 = " + file.getAbsolutePath() );
-                    * /
+                });
+            //log.debug ( "MONA : subdirs = " + subdirs );
+
+            for ( String subdir : subdirs )
+            {
+                //log.debug ( "MONA : subdir = " + subdir );
+                File file = new File ( full_path + "/" + subdir +
+                    "/particles.star" );
+                //log.debug ( "MONA : file = " + file );
+                if ( file.exists() )
+                {
+                    log.debug ( "file exists!" );
+                    dir = new File ( full_path + "/" + subdir );
+                    //log.debug ( "MONA : dir = " + dir );
+
                     String label = file.toString().substring
                         ( label_start_index );
-                    log.debug ( "MONA : label = " + label );
+                    //log.debug ( "MONA : label = " + label );
                     try
                     {
-                        data_item = new UserDataDirItem ( folder, tr_id,
-                            label, file.length() );
-                        log.debug ( "MONA : data_item = " + data_item );
+                        data_item = new UserDataDirItem ( folder, tr_id, label,
+                            FileUtils.sizeOf ( dir ) );
+                        //log.debug ( "MONA : data_item = " + data_item );
                         if ( data_item != null )
                         {
                             data_item.save();
@@ -215,22 +197,21 @@ public class Transfer2DataManager extends NgbwSupport
                     }
                     catch ( Exception e )
                     {
-                        reportUserError (
-                            "Unable to setup Globus transferred data item (" +
-                            e + ")" );
-                        addActionError ( 
-                            "Unable to setup Globus transferred data item (" +
-                            e + ")" );
+                        String msg = 
+                            "Unable to setup Globus transferred data item for file "
+                            + file.getName() + " (" + e + ")";
+                        reportUserError ( msg );
+                        addActionError ( msg ); 
                         //reportError(error, "Error creating new TaskInputSourceDocument");
                         log.error
-                            ( "System Error : cannot create data item for Globus transferred directory ("
-                            + e + ")" );
+                            ( "System Error : cannot create data item for Globus data item "
+                              + file.getName() + " (" + e + ")" );
                     }
                 }
             }
-            */
         }
 
+        //log.debug ( "MONA : saved = " + saved );
         return ( saved );
     }
 
