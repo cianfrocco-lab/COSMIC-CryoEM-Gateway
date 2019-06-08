2e0425b185885a97aaa566aa2f42a0ef8c5e02c7
Mona Wong <mona@sdsc.edu>
2019-06-07 17:24:05 -0700

    Removed unused WorkbenchSession import

--- gateway_config/portal/src/main/java/edu/sdsc/globusauth/controller/ProfileManager.java
+++ gateway_config/portal/src/main/java/edu/sdsc/globusauth/controller/ProfileManager.java
@@ -15,10 +15,9 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.ngbw.sdk.Workbench;
-import org.ngbw.sdk.WorkbenchSession;
 
 import edu.sdsc.globusauth.controller.Transfer2DataManager;
+import org.ngbw.sdk.Workbench;
 import org.ngbw.sdk.database.OauthProfile;
 import org.ngbw.sdk.database.TransferRecord;
 
