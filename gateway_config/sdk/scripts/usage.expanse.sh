dfb1e1f13c137eb64cf04ff482488b86971b8aa9
COSMIC2 Community User <cosmic2-gw@cosmic2dev.sdsc.edu>
2021-01-21 10:10:12 -0800

    expanse-specific usage scripts

---
+++ gateway_config/sdk/scripts/usage.expanse.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#source $HOME/.bashrc
+export PATH=/usr/sbin:$PATH:$SDK_VERSIONS
+TODAY=`date +%m-%d-%Y`
+#CIPRES_CHARGE=TG-DEB090011
+CIPRES_CHARGE=csd547
+CIPRES_CHARGE=TG-MCB170058
+#IPLANT_CHARGE=TG-MCB110022
+
+#source /users/u2/cosmic2-gw/gatewayVE/bin/activate
+
+cd ${SDK_VERSIONS}
+if test ! -d tgusage.expanse
+then
+  mkdir tgusage.expanse
+fi
+cd ${SDK_VERSIONS}/tgusage.expanse
+tgusage.expanse.sh $1 
+
+usage.py -j  
+usage.py -c $CIPRES_CHARGE -a > cipres_tg_jobs.csv
+usage.py -c $CIPRES_CHARGE -t > cipres_su_per_user_total.csv
+usage.py -c $CIPRES_CHARGE -p > cipres_su_this_period.csv
+
+usage.py -e -c $CIPRES_CHARGE -t > cipres_su_per_email_total.csv
+usage.py -e -c $CIPRES_CHARGE -p > cipres_su_per_email_this_period.csv
+
+# usage.py -c $CIPRES_CHARGE -u > cipres_su_per_user_$TODAY.csv
+
+#usage.py -c $IPLANT_CHARGE -a > iplant_tg_jobs.csv
+#usage.py -c $IPLANT_CHARGE -t > iplant_su_per_user_total.csv
+#usage.py -c $IPLANT_CHARGE -p > iplant_su_this_period.csv
+
+# usage.py -c $IPLANT_CHARGE -u > iplant_su_per_user_$TODAY.csv
+
+#usage.py -c $CIPRES_CHARGE -w 
+
