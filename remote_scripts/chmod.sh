218dd98329a688f13d7ed6c9ddda34173518bc76
COSMIC2 Community User <cosmic2@login02.expanse.sdsc.edu>
2022-11-23 09:21:02 -0800

    Take -print out of chmod in chmod.sh

--- remote_scripts/chmod.sh
+++ remote_scripts/chmod.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#sleep 3600
 . /expanse/projects/cosmic2/expanse/gatewaydev/bashrc.dev
-find "$1" -type d -exec chmod g+rwx {} \; -print
-find "$1" -type f -exec chmod g+rw {} \; -print
+#find "$1" -type d -exec chmod g+rwx {} \; -print
+#find "$1" -type f -exec chmod g+rw {} \; -print
+find "$1" -type d -exec chmod g+rwx {} \;
+find "$1" -type f -exec chmod g+rw {} \;
